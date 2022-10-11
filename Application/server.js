4d372f870013ed9153224ffcc1489795e29a362b
Coki3310 <80317677+Coki3310@users.noreply.github.com>
2022-10-11 10:56:01 +0200

    CorrectionBugs

--- Application/server.js
+++ Application/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
+const {exec} = require('child_process')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,5 +12,15 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "/dist/projet-esme/index.html"));
     console.log('test')
 });
-
+app.get('/Python', function(req,res){
+    exec('python ./authentification.py', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+      res.sendFile(path.join(__dirname, "/dist/projet-esme/index.html"));
+});
 app.listen(4200)
